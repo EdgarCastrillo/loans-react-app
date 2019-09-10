795e7e83904d16b7575dfb3383c3b28a3dd3d4ed
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-10 14:41:43 +0200

    disable button submit and read the inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,51 @@ import React, { Component } from 'react';
 
 class Form extends Component {
   state = { 
+    cantidad: '',
+    plazo: ''
+  }
 
-   }
+
+  actualizarState = (e) => {
+    // Leer los datos del form
+    // console.log(e.target.value)
+    const { name, value } = e.target 
+
+    // Actualizar el State
+    this.setState({
+      [name] : Number(value)
+    })
+
+  }
+
+  habilitarSubmit = () => {
+    // Aplicar destructuring
+    const { cantidad, plazo } = this.state
+    // leer las variables 
+    const noValido = !cantidad || !plazo
+    console.log(noValido)
+    // Retornar una respuesta
+    return noValido
+  }
+
 
   render() { 
+    const { cantidad } = this.state
+    // console.log(cantidad)
     return (
       <form>
         <div>
-          <label>Cantidad Prestamo:</label>
-          <input type="number" name="cantidad" className="u-full-width" placeholder="Ejemplo: 3000" />
+          <label>Cantidad Prestamo: {cantidad}</label>
+          <input
+            onChange={this.actualizarState}
+            type="number"
+            name="cantidad"
+            className="u-full-width"
+            placeholder="Ejemplo: 3000" />
         </div>
         <div>
           <label>Plazo para pagar:</label>
-          <select name="plazo" className="u-full-width">
+          <select onChange={this.actualizarState} name="plazo" className="u-full-width">
             <option value="">Seleccionar</option>
             <option value="3">3 Meses</option>
             <option value="6">6 Meses</option>
@@ -24,7 +56,11 @@ class Form extends Component {
           </select>
         </div>
         <div>
-          <input type="submit" value="calcular" className="u-full-width button-primary" />
+          <input 
+            disabled={this.habilitarSubmit()}
+            type="submit"
+            value="calcular"
+            className="u-full-width button-primary" />
         </div>
       </form>
     );
