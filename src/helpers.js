46f41ba3faad599c6b585c3e2a13924d224c756c
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-10 15:16:57 +0200

    calcular comision prestamo-plazo

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,6 @@
 export function calcularTotal(cantidad, plazo) {
   
   // Cantidades
-  // 0 a 1000 = 25%
-  // 1001 a 5000 = 20%
-  // 5001 a 10000 = 15%
-  // +10001 = 10%
-
   let totalCantidad
 
   if(cantidad <= 1000) {
@@ -17,12 +12,30 @@ export function calcularTotal(cantidad, plazo) {
   } else {
     totalCantidad = cantidad * 0.10
   }
-
   console.log(totalCantidad)
 
   // Meses
-  // 3 = 5%
-  // 6 = 10%
-  // 12 = 15%
-  // 24 = 20%
+  let totalPlazo
+
+  switch (plazo) {
+    case 3:
+      totalPlazo = cantidad * 0.05
+      break;
+    case 6:
+      totalPlazo = cantidad * 0.10
+      break;
+    case 12:
+      totalPlazo = cantidad * 0.15
+      break;
+    case 24:
+      totalPlazo = cantidad * 0.20
+      break;
+  
+    default:
+      break;
+  }
+  console.log(totalPlazo)
+  console.log(cantidad + totalPlazo + totalCantidad)
+
+  return cantidad + totalPlazo + totalCantidad
 }
