6c271f92e1013994997f5d7dcde3e153c6b8ddc1
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-10 14:14:25 +0200

    create form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component, Fragment} from 'react';
 
 import './normalize.css';
-import './skeleton.css';
+import './skeleton.css'
 
-function App() {
-  return (
-    <h2>Hola</h2>
-  );
+import Form from './components/Form'
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Calcular prestamo</h1>
+        <div className="container">
+          <Form />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = { 
+
+   }
+
+  render() { 
+    return (
+      <form>
+        <div>
+          <label>Cantidad Prestamo:</label>
+          <input type="number" name="cantidad" className="u-full-width" placeholder="Ejemplo: 3000" />
+        </div>
+        <div>
+          <label>Plazo para pagar:</label>
+          <select name="plazo" className="u-full-width">
+            <option value="">Seleccionar</option>
+            <option value="3">3 Meses</option>
+            <option value="6">6 Meses</option>
+            <option value="12">12 Meses</option>
+            <option value="24">24 Meses</option>
+          </select>
+        </div>
+        <div>
+          <input type="submit" value="calcular" className="u-full-width button-primary" />
+        </div>
+      </form>
+    );
+  }
+}
+ 
+export default Form;
